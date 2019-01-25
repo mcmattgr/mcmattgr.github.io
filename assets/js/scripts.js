d18cf8c4904e83551e826ee7d9d111c8ee4d72f5
mcmattgr <mattmclennan@yahoo.com>
2019-01-25 09:59:25 -0500

    show hide completed on text descriptions

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,15 +6,22 @@ $('#drag-man').draggable({helper:'clone'});  // makes top images draggable
 
 $(".quid-contain, .hostile-contain").droppable({  //makes contents in div droppable
   drop: function(e, ui) {
-    var idArray = ["drag-woman", "drag-man"];$('#drag-woman').draggable({helper:'clone'});
-$('#drag-man').draggable({helper:'clone'});  // makes top images draggable
+    var idArray = ["drag-woman", "drag-man"];
+
+$('#drag-woman').draggable({helper:'clone'});
+$('#drag-man').draggable({helper:'clone'}); 
+
     if((idArray.indexOf(ui.draggable.attr("id")) + 1) && $(this).hasClass("quid-contain")) {  //if id is dragged do this
 
        $('.quid-with-' + ui.draggable.attr("id").substring(5)).show().siblings().hide();
+       $('#quid-text').show();
+       $('#hostile-text').hide();
 
     }else if ((idArray.indexOf(ui.draggable.attr("id")) + 1) && $(this).hasClass("hostile-contain")){  // else if dragged do this
 
        $('.hostile-with-' + ui.draggable.attr("id").substring(5)).show().siblings().hide();
+       $('#hostile-text').show();
+       $('#quid-text').hide();
     }
   }
 });

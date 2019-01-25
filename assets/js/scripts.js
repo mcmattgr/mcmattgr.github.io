960c6bdb964573870770ff0566241b3cb7a4c839
mcmattgr <mattmclennan@yahoo.com>
2019-01-24 20:23:22 -0500

    adding test images and reworking js for draggable

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,2 +1,16 @@
 // Scripts.js
 
+
+$('#element1').draggable({helper:'clone'});
+$('#element2').draggable({helper:'clone'});
+$("#images").droppable({
+  drop: function(e, ui) {
+    if((ui.draggable.attr("id")) == 'element1'){
+       $('.element2').hide();
+       $('.element1').show();
+    }else if((ui.draggable.attr("id")) == 'element2'){
+       $('.element1').hide();
+       $('.element2').show();
+    }
+  }
+});

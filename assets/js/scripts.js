a2389f43a91584429d0d7d04c1d5c39488b98fd8
mcmattgr <mattmclennan@yahoo.com>
2019-01-25 00:57:42 -0500

    Working Solution for image swap, text next

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,16 +1,20 @@
 // Scripts.js
 
 
-$('#element1').draggable({helper:'clone'});
-$('#element2').draggable({helper:'clone'});
-$("#images").droppable({
+$('#drag-woman').draggable({helper:'clone'});
+$('#drag-man').draggable({helper:'clone'});  // makes top images draggable
+
+$(".quid-contain, .hostile-contain").droppable({  //makes contents in div droppable
   drop: function(e, ui) {
-    if((ui.draggable.attr("id")) == 'element1'){
-       $('.element2').hide();
-       $('.element1').show();
-    }else if((ui.draggable.attr("id")) == 'element2'){
-       $('.element1').hide();
-       $('.element2').show();
+    var idArray = ["drag-woman", "drag-man"];$('#drag-woman').draggable({helper:'clone'});
+$('#drag-man').draggable({helper:'clone'});  // makes top images draggable
+    if((idArray.indexOf(ui.draggable.attr("id")) + 1) && $(this).hasClass("quid-contain")) {  //if id is dragged do this
+
+       $('.quid-with-' + ui.draggable.attr("id").substring(5)).show().siblings().hide();
+
+    }else if ((idArray.indexOf(ui.draggable.attr("id")) + 1) && $(this).hasClass("hostile-contain")){  // else if dragged do this
+
+       $('.hostile-with-' + ui.draggable.attr("id").substring(5)).show().siblings().hide();
     }
   }
 });
